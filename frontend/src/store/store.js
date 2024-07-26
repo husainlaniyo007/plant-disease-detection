d61fcb4811669a43901fb5de23852e63cf60e5aa
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 03:21:28 -0700

    Update store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,1 +1,13 @@
+import { createStore, combineReducers } from 'redux';
+import imageReducer from './reducers/imageReducer';
+import userReducer from './reducers/userReducer';
+
+const rootReducer = combineReducers({
+  images: imageReducer,
+  user: userReducer
+});
+
+const store = createStore(rootReducer);
+
+export default store;
 
