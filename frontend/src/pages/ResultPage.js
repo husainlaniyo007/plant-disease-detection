3ac69fa0ad6f4989b83a5e84b3690b892a24c934
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 03:00:04 -0700

    Update ResultPage.js

--- frontend/src/pages/ResultPage.js
+++ frontend/src/pages/ResultPage.js
@@ -1,1 +1,18 @@
 
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ResultDisplay from '../components/ResultDisplay';
+
+const ResultPage = () => {
+  const result = {}; // Example result
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Result Page</h1>
+      <ResultDisplay result={result} />
+    </div>
+  );
+};
+
+export default ResultPage;
