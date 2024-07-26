cdb2363d2e9358499f3187c421c0b9df3de38d19
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 02:56:27 -0700

    Update uploadimage.js

--- frontend/src/assets/images/uploadimage.js
+++ frontend/src/assets/images/uploadimage.js
@@ -1,1 +1,16 @@
 
+import React from 'react';
+
+const ImageUpload = () => {
+  const handleUpload = (event) => {
+    // handle image upload
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleUpload} />
+    </div>
+  );
+};
+
+export default ImageUpload;
