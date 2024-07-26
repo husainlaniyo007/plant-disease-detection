fb737653150bcaee70a0ea6771e5306353901336
Mohbayuekaputra <mohbayue@gmail.com>
2024-07-26 18:00:57 +0800

    Update helpers.js

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,1 +1,38 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Generate a hashed password
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
+// Compare a password with a hashed password
+const comparePassword = async (password, hashedPassword) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+// Generate a JWT token
+const generateToken = (user) => {
+  const payload = { user: { id: user.id } };
+  const token = jwt.sign(payload, 'your_jwt_secret', { expiresIn: '1h' });
+  return token;
+};
+
+// Verify a JWT token
+const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, 'your_jwt_secret');
+  } catch (err) {
+    return null;
+  }
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+  generateToken,
+  verifyToken,
+};
 
