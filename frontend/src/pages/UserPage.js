83e3a2571b3bac8080ebff02f2e816f94d8d4c0d
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 03:00:27 -0700

    Update UserPage.js

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const UserPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>User Page</h1>
+    </div>
+  );
+};
+
+export default UserPage;
 
