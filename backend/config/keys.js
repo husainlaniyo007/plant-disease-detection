5c55d5ee0f84c0313d6413ba0002f8d528255664
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 02:30:51 -0700

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,1 +1,20 @@
+// keys.js
+
+module.exports = {
+  database: {
+    host: 'localhost',
+    user: 'your_username',
+    password: 'your_password',
+    database: 'your_database'
+  },
+  apiKeys: {
+    google: 'your_google_api_key',
+    aws: 'your_aws_api_key',
+    // Add other API keys as needed
+  },
+  secretKeys: {
+    jwtSecret: 'your_jwt_secret_key',
+    // Add other secret keys as needed
+  }
+};
 
