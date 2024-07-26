f1ab89ccc96af3527789940cf14f8f6f4099791c
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 02:57:06 -0700

    Update ResultDisplay.js

--- frontend/src/components/ResultDisplay.js
+++ frontend/src/components/ResultDisplay.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+const ResultDisplay = ({ result }) => {
+  return (
+    <div>
+      <h2>Results</h2>
+      <pre>{JSON.stringify(result, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default ResultDisplay;
 
