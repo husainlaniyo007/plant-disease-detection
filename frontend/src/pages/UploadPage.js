300751fdcd5ddb37a51e2d7d4886431cbcd7c5fb
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 02:59:38 -0700

    Update UploadPage.js

--- frontend/src/pages/UploadPage.js
+++ frontend/src/pages/UploadPage.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import ImageUpload from '../components/ImageUpload';
+
+const UploadPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Upload Page</h1>
+      <ImageUpload />
+    </div>
+  );
+};
+
+export default UploadPage;
 
