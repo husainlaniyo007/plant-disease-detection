b06ffabede098a7732ae451fceed18fc8a30af9a
Mohbayuekaputra <mohbayue@gmail.com>
2024-07-26 17:48:16 +0800

    Update errorMiddleware.js

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const app = express();
+
+// Simple logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next(); // Pass the request to the next middleware
+});
+
+// Middleware to add a custom header
+app.use((req, res, next) => {
+  res.setHeader('X-Custom-Header', 'CustomHeaderValue');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
