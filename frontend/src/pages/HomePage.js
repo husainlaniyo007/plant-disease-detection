8a2b718a2d31f7bb860468e6524162bdf3f5407a
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 02:59:18 -0700

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,14 @@
 
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
