febad7f5ed2fa4fd39e69ace3c58e6c10a7dc3b5
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 03:07:54 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import UploadPage from './pages/UploadPage';
+import ResultPage from './pages/ResultPage';
+import UserPage from './pages/UserPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/upload" component={UploadPage} />
+        <Route path="/results" component={ResultPage} />
+        <Route path="/user" component={UserPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
 
