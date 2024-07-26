f610d99780c8b9218e238ff0390460b5dbf1a60c
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 03:20:23 -0700

    Update imageReducer.js

--- frontend/src/store/reducers/imageReducer.js
+++ frontend/src/store/reducers/imageReducer.js
@@ -1,1 +1,18 @@
+const initialState = {
+  images: []
+};
+
+const imageReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'UPLOAD_IMAGE':
+      return {
+        ...state,
+        images: [...state.images, action.payload]
+      };
+    default:
+      return state;
+  }
+};
+
+export default imageReducer;
 
