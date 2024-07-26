3244ae97414aa7d13b23889d959ff132a4a67d1d
husainlaniyo007 <ryuzinproject@gmail.com>
2024-07-26 02:58:35 -0700

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/upload">Upload</Link></li>
+        <li><Link to="/results">Results</Link></li>
+        <li><Link to="/user">User</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
 
